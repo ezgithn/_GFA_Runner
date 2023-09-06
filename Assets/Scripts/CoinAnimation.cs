45225b49e3b480211cb33c0c143262c8db0bfd88
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-09-07 02:56:55 +0300

    CoinAnim.

---
+++ Assets/Scripts/CoinAnimation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class CoinAnimation : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.DOLocalMoveY(1, 1).SetLoops(-1, LoopType.Yoyo).SetDelay(transform.position.z %2 * 0.1f);
+        transform.DOLocalRotate(Vector3.up * 360,2.5f, RotateMode.WorldAxisAdd).SetLoops(-1).SetEase(Ease.Linear);
+    }
+    
+    private void OnDestroy()
+    {
+        transform.DOKill();
+    }
+}
