34cfcd814e981f90802b78fc972831e8c65c38c7
ezgithn <108123991+ezgithn@users.noreply.github.com>
2023-10-20 22:48:40 +0300

    LevelSets.

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -2,14 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+	public float timeToNextLevel = 2f; 
+
+	private bool _levelCompleted = false;
+	
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.attachedRigidbody.CompareTag("Player"))
+		if (other.attachedRigidbody.CompareTag("Player") && !_levelCompleted)
 		{
+			_levelCompleted = true;
 			GameInstance.Instance.Win();
+			StartCoroutine(NextLevelTimer());
 		}
+	}
+	
+	IEnumerator NextLevelTimer()
+	{
+		yield return new WaitForSeconds(timeToNextLevel);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 }
